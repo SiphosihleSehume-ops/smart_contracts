119acf5f8a7bb817b8ad1aa1c3174ee3a47e920e
Siphesihle Sehume <siphosihlesehume@gmail.com>
2026-09-23 23:00:19 +0200

    ADD: user struct + setName functionality

--- contracts/SimpleContract.sol
+++ contracts/SimpleContract.sol
@@ -3,5 +3,29 @@ pragma solidity ^0.8.27;
 
 contract SimpleContract {
 
-    constructor() {}
+    address public owner;
+
+    struct Owner {
+        string name;
+        string balance;
+        bool isActive;
+        // datetime 
+    }
+
+    Owner public user;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier OnlyOwner {
+        require(msg.sender == owner, "Only the owner has rights to this action");
+        _;
+    }
+
+    // Owner can change their name
+    function setName(string calldata _name) external OnlyOwner {
+        user.name = _name;
+    }
+
 }
